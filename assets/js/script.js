36cc61c6676190ff3b8eb263afb6a8cbcb6da9eb
pantspower <hello.kerrin@gmail.com>
2023-03-26 22:34:10 -0400

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+function dontClick() {
+  alert("I told you not to click this! Bye.");
+  window.close();
+}
+
+function addingMachine() {
+  
+  let number1 = Number(document.getElementById("number1").value);
+  let number2 = Number(document.getElementById("number2").value);
+
+  if (number1 != null && number2 != null) {
+    let output = number1 + number2;
+    document.getElementById("output").innerHTML = output;
+  }
+}
+
+function clear() {
+    document.getElementById("number1").value = "";
+    document.getElementById("number2").value = "";
+    document.getElementById("output").innerHTML = ""; 
+}
+
+function meow(){
+    const meowSound = new Audio('assets/audio/cat-meow-6226.mp3');
+    meowSound.play();
+}
+
+let darkModeSetting = false;
+function darkMode() {
+ if(darkModeSetting == false ) {
+    darkModeSetting = true;
+    document.body.style.backgroundColor = "black";
+    document.body.style.color = "white";
+    document.getElementById("dark-mode").innerHTML = "Deactivate Dark Mode";
+
+ }
+ else {
+     darkModeSetting = false;
+     document.body.style.backgroundColor = "white";
+     document.body.style.color = "black";
+     document.getElementById("dark-mode").innerHTML = "Activate Dark Mode";
+ }
+}
